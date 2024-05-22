45e99864d8fe79128d1c50c700c397700588198a
Jakub Ziolkowski <jakubziolkowski@digiproject.uk>
2024-05-22 08:39:27 +0200

    Formula Upgraded to .NET Standard

--- Formula/Classes/FormulaCalculator.cs
+++ Formula/Classes/FormulaCalculator.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
-using System.Runtime.Remoting.Messaging;
+//using System.Runtime.Remoting.Messaging;
 
 namespace Formula
 {
